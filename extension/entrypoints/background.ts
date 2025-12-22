af15802824d92337fbd5514104e7703c2f8c914a
Rainnand <xinzhao2627@gmail.com>
2025-12-22 11:44:51 +0800

    added recursive fetcher

--- extension/entrypoints/background.ts
+++ extension/entrypoints/background.ts
@@ -1,6 +1,30 @@
 import jsonQuery from "json-query";
+function recursive_replies_fetcher(replies: any, collections: string[]) {
+	if (!replies) {
+		return;
+	}
+	const REMOVED = "[removed]";
+	const DELETED = "[deleted]";
 
+	const bodySection = replies["data"]["children"];
+
+	for (const b of bodySection) {
+		const data = b["data"];
+		const text = data["body"];
+
+		if (text && text !== REMOVED && text !== DELETED) {
+			collections.push(text);
+		}
+
+		// if a reply has a reply, loop it
+		if (data["replies"]) {
+			recursive_replies_fetcher(data["replies"], collections);
+		}
+	}
+}
 function getBody(data: any, collections: string[]): string[] {
+	const REMOVED = "[removed]";
+	const DELETED = "[deleted]";
 	const titleSection = data[0]["data"]["children"][0]["data"];
 
 	const title = titleSection["title"] as string;
@@ -14,7 +38,13 @@ function getBody(data: any, collections: string[]): string[] {
 	for (const comment of commentsSection) {
 		const text = comment["data"]["body"] as string;
 
-		if (text) {
+		const replies = comment["data"]["replies"];
+
+		if (replies) {
+			recursive_replies_fetcher(replies, collections);
+		}
+
+		if (text && text !== REMOVED && text !== DELETED) {
 			collections.push(text);
 		}
 	}
@@ -24,6 +54,7 @@ function getBody(data: any, collections: string[]): string[] {
 
 export default defineBackground(() => {
 	// console.log("Hello background!", { id: browser.runtime.id });
+
 	const BACKEND_URL =
 		import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
 
@@ -60,12 +91,23 @@ export default defineBackground(() => {
 							// send the json to springboot server
 							fetch(BACKEND_URL, {
 								method: "POST",
-								body: JSON.stringify({ data: result }),
+								body: JSON.stringify({ data: collections }),
 								headers: {
 									"Content-type":
 										"application/json; charset-UTF-8",
 								},
-							});
+							})
+								.then((r) => {
+									console.log("\n");
+									console.log(r.status);
+									return r.text();
+								})
+								.then((r) => {
+									console.log("final r: ", r);
+								})
+								.catch((e) => {
+									console.log("ERROR POST REQ: ", e);
+								});
 						}
 					}
 				});
